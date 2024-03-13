d205d4b2d7b2a5ab2986e44bb61cb7c7a802ada6
Anmol Manchanda <manchandaanmol@icloud.com>
2024-03-13 16:20:46 -0400

    Initial Commit

---
+++ Wallet/WalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletApp.swift
+//  Wallet
+//
+//  Created by Anmol Manchanda on 2024-03-13.
+//
+
+import SwiftUI
+
+@main
+struct WalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
